924dc6c0d10deeba6e918b828d9270289a7d54f1
ZhengChangfu <zcfpromise@163.com>
2024-12-14 18:02:37 +0800

    chore: 更新依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { analyzer } from 'vite-bundle-analyzer'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(), 
+    // analyzer()
+  ],
   optimizeDeps: {
     exclude: ['@vue/repl'],
   },
