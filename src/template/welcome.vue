775ec123f3cbf79c31d6eec0498afd8f556f0b7e
liyuan26 <liyuan26@daojia-inc.com>
2024-12-18 19:23:37 +0800

    fix: 修改初始模板及版本包

---
+++ src/template/welcome.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+import { ProCard } from 'pro-naive-ui'
+
+const msg = ref('Hello World!')
+</script>
+
+<template>
+  <pro-card title="Welcome">
+    <span class="c-red">{{ msg }}</span>
+  </pro-card>
+</template>
